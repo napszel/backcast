53482b3009efcbe3acb329bc228112cec73ce8ec
Eva Szadeczky-Kardoss <eva@napszel.com>
2020-08-09 16:57:38 +0200

    SQL and backend design

---
+++ backend/db.sql
@@ -0,0 +1,19 @@
+CREATE TABLE City (
+	  lon FLOAT
+	, lat FLOAT
+	, name TEXT
+);
+
+CREATE TABLE Forecast (
+	  date_taken TEXT
+	, date_for TEXT
+	, city_id INT NOT NULL
+	, temp_min FLOAT
+	, temp_max FLOAT
+	, weather_desc TEXT
+	, FOREIGN KEY (city_id) REFERENCES City (ROWID)
+);
+
+insert into City (lon, lat, name) VALUES (8.55, 47.36667, "Zurich");
+insert into City (lon, lat, name) VALUES (19.0402, 47.4979, "Budapest");
+insert into City (lon, lat, name) VALUES (19.945, 50.0647, "Krakow");
