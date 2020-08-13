b48dc99baf861a5474e640a3eafd75370cfeef39
Eva Szadeczky-Kardoss <eva@napszel.com>
2020-08-14 00:30:30 +0200

    icon less text print to ui

---
+++ javascript.js
@@ -0,0 +1,26 @@
+function print_weather_data(today, city) {
+  forecasts = weather_data[today][city]
+  $("#weather_data").append("<p id=\"" + city + "\">" + city + ": </p>" );
+  var i;
+  for (i = 0; i < forecasts.length; i++) {
+    $("#" + city).append(
+      "<label>"
+	+ "<b>" + forecasts[i]["date_taken"] + "</b> "
+	+ "Min: " + forecasts[i]["temp_min"] + " "
+	+ "Max: " + forecasts[i]["temp_max"]
+	+ "</label> | " );
+  }
+}
+
+$(document).ready(function() {
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0');
+  var yyyy = today.getFullYear();
+
+  today = yyyy + '-' + mm + '-' + dd;
+
+  print_weather_data(today, "Zurich");
+  print_weather_data(today, "Budapest");
+  print_weather_data(today, "Krakow");
+} );
