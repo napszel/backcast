a0f3431512dcde02f7f41d7c0705f539eff89587
Eva Szadeczky-Kardoss <eva@napszel.com>
2020-08-23 22:16:43 +0200

    cities as tabs; formatting

--- javascript.js
+++ javascript.js
@@ -1,15 +1,20 @@
+
 function print_weather_data(today, city) {
   forecasts = weather_data[today][city]
-  $("#weather_data").append("<p id=\"" + city + "\">" + city + ": </p>" );
+
+  $("#title").html(city);
+
+  $("#weather_data").empty();
   var i;
   for (i = 0; i < forecasts.length; i++) {
-    $("#" + city).append(
-      "<label>"
-	+ "<b>" + forecasts[i]["date_taken"] + "</b> "
-        + "<i class=\"" + forecasts[i]["weather_icon"] + "\"></i>"
-	+ "Min: " + forecasts[i]["temp_min"] + " "
-	+ "Max: " + forecasts[i]["temp_max"]
-	+ "</label> | " );
+    $("#weather_data").append(
+      "<div>"
+	+ "<label class='date'>" + forecasts[i]["date_taken"] + "</label>  "
+        + forecasts[i]["weather_desc"]
+        + "    <i class=\"" + forecasts[i]["weather_icon"] + "\"></i>    "
+	+ Math.round(forecasts[i]["temp_min"]) + "&#8451; &ndash; "
+	+ Math.round(forecasts[i]["temp_max"]) + "&#8451;"
+	+ "</div><br/>" );
   }
 }
 
@@ -22,6 +27,16 @@ $(document).ready(function() {
   today = yyyy + '-' + mm + '-' + dd;
 
   print_weather_data(today, "Zurich");
-  print_weather_data(today, "Budapest");
-  print_weather_data(today, "Krakow");
+
+  $("#zurich").click(function() {
+    print_weather_data(today, "Zurich");
+  });
+
+  $("#budapest").click(function() {
+    print_weather_data(today, "Budapest");
+  });
+
+  $("#krakow").click(function() {
+    print_weather_data(today, "Krakow");
+  });
 } );
